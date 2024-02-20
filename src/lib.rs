76f33c62e1386776170b996441728973452de7e5
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-02-21 01:22:43 +0500

    implements simple get_stats function in rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 use pyo3::prelude::*;
+use std::collections::HashMap;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn get_stats(
+    ids: Vec<usize>,
+    counts: Option<HashMap<(usize, usize), usize>>,
+) -> PyResult<HashMap<(usize, usize), usize>> {
+    let mut counts = counts.unwrap_or(HashMap::new());
+    for pair in ids.windows(2) {
+        // rustic way to iterate consecutive elements ;)
+        *counts.entry((pair[0], pair[1])).or_insert(0) += 1;
+    }
+    Ok(counts)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _minbpe(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(get_stats, m)?)?;
     Ok(())
 }
