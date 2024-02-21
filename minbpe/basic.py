18a25d2762da40900ef9c55930ded5ca3703653d
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-02-21 10:24:25 +0500

    rust code

--- minbpe/basic.py
+++ minbpe/basic.py
@@ -9,11 +9,13 @@ But:
 - Does not handle any special tokens.
 """
 
-from .base import Tokenizer, get_stats, merge
+# from .base import Tokenizer, get_stats, merge
+from _minbpe import get_stats, merge, train_basic_bpe
+
+from .base import Tokenizer
 
 
 class BasicTokenizer(Tokenizer):
-
     def __init__(self):
         super().__init__()
 
@@ -22,31 +24,36 @@ class BasicTokenizer(Tokenizer):
         num_merges = vocab_size - 256
 
         # input text preprocessing
-        text_bytes = text.encode("utf-8") # raw bytes
-        ids = list(text_bytes) # list of integers in range 0..255
+        text_bytes = text.encode("utf-8")  # raw bytes
+        ids = list(text_bytes)  # list of integers in range 0..255
 
-        # iteratively merge the most common pairs to create new tokens
-        merges = {} # (int, int) -> int
-        vocab = {idx: bytes([idx]) for idx in range(256)} # int -> bytes
-        for i in range(num_merges):
-            # count up the number of times every consecutive pair appears
-            stats = get_stats(ids)
-            # find the pair with the highest count
-            pair = max(stats, key=stats.get)
-            # mint a new token: assign it the next available id
-            idx = 256 + i
-            # replace all occurrences of pair in ids with idx
-            ids = merge(ids, pair, idx)
-            # save the merge
-            merges[pair] = idx
-            vocab[idx] = vocab[pair[0]] + vocab[pair[1]]
-            # prints
-            if verbose:
-                print(f"merge {i+1}/{num_merges}: {pair} -> {idx} ({vocab[idx]}) had {stats[pair]} occurrences")
+        merges, vocab = train_basic_bpe(ids, num_merges)
+
+        print(merges)
+        print(vocab)
+
+        # # iteratively merge the most common pairs to create new tokens
+        # merges = {} # (int, int) -> int
+        # vocab = {idx: bytes([idx]) for idx in range(256)} # int -> bytes
+        # for i in range(num_merges):
+        #     # count up the number of times every consecutive pair appears
+        #     stats = get_stats(ids)
+        #     # find the pair with the highest count
+        #     pair = max(stats, key=stats.get)
+        #     # mint a new token: assign it the next available id
+        #     idx = 256 + i
+        #     # replace all occurrences of pair in ids with idx
+        #     ids = merge(ids, pair, idx)
+        #     # save the merge
+        #     merges[pair] = idx
+        #     vocab[idx] = vocab[pair[0]] + vocab[pair[1]]
+        #     # prints
+        #     if verbose:
+        #         print(f"merge {i+1}/{num_merges}: {pair} -> {idx} ({vocab[idx]}) had {stats[pair]} occurrences")
 
         # save class variables
-        self.merges = merges # used in encode()
-        self.vocab = vocab   # used in decode()
+        self.merges = merges  # used in encode()
+        self.vocab = vocab  # used in decode()
 
     def decode(self, ids):
         # given ids (list of integers), return Python string
@@ -56,8 +63,8 @@ class BasicTokenizer(Tokenizer):
 
     def encode(self, text):
         # given a string text, return the token ids
-        text_bytes = text.encode("utf-8") # raw bytes
-        ids = list(text_bytes) # list of integers in range 0..255
+        text_bytes = text.encode("utf-8")  # raw bytes
+        ids = list(text_bytes)  # list of integers in range 0..255
         while len(ids) >= 2:
             # find the pair with the lowest merge index
             stats = get_stats(ids)
@@ -67,7 +74,7 @@ class BasicTokenizer(Tokenizer):
             # just the first pair in the list, arbitrarily
             # we can detect this terminating case by a membership check
             if pair not in self.merges:
-                break # nothing else can be merged anymore
+                break  # nothing else can be merged anymore
             # otherwise let's merge the best pair (lowest merge index)
             idx = self.merges[pair]
             ids = merge(ids, pair, idx)
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,86 @@
-use pyo3::prelude::*;
+use pyo3::{
+    prelude::*,
+    types::{PyBytes, PyDict},
+};
 use std::collections::HashMap;
 
-#[pyfunction]
 fn get_stats(
     ids: Vec<usize>,
     counts: Option<HashMap<(usize, usize), usize>>,
-) -> PyResult<HashMap<(usize, usize), usize>> {
+) -> Result<HashMap<(usize, usize), usize>, ()> {
     let mut counts = counts.unwrap_or(HashMap::new());
     for pair in ids.windows(2) {
-        // rustic way to iterate consecutive elements ;)
         *counts.entry((pair[0], pair[1])).or_insert(0) += 1;
     }
     Ok(counts)
 }
 
+fn merge(ids: Vec<usize>, pair: (usize, usize), idx: usize) -> Result<Vec<usize>, ()> {
+    let mut newids = vec![];
+    let mut i = 0;
+    while i < ids.len() {
+        if ids[i] == pair.0 && i < ids.len() - 1 && ids[i + 1] == pair.1 {
+            newids.push(idx);
+            i += 2;
+        } else {
+            newids.push(ids[i]);
+            i += 1;
+        }
+    }
+    Ok(newids)
+}
+
+#[pyfunction]
+fn train_basic_bpe(
+    ids: Vec<usize>,
+    num_merges: usize,
+) -> PyResult<(HashMap<(usize, usize), usize>, Py<PyDict>)> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+
+    let mut merges = HashMap::new();
+    let mut ids = ids;
+    let mut vocab = PyDict::new(py);
+
+    for idx in 0..256 {
+        let bytes_value = PyBytes::new(py, &[idx as u8])?;
+        vocab.set_item(idx, bytes_value)?;
+    }
+
+    for i in 0..num_merges {
+        let stats = get_stats(ids.clone(), None)?;
+        let pair = *stats.iter().max_by_key(|(_, &v)| v).unwrap().0;
+        let idx = 256 + i;
+        ids = merge(ids, pair, idx)?;
+        merges.insert(pair, idx);
+
+        let mut new_vocab = vec![];
+        new_vocab.extend_from_slice(
+            &vocab
+                .get_item(pair.0)
+                .unwrap()
+                .extract::<PyBytes>(py)?
+                .as_bytes(),
+        );
+        new_vocab.extend_from_slice(
+            &vocab
+                .get_item(pair.1)
+                .unwrap()
+                .extract::<PyBytes>(py)?
+                .as_bytes(),
+        );
+
+        let bytes_value = PyBytes::new(py, &new_vocab)?;
+        vocab.set_item(idx, bytes_value)?;
+    }
+
+    Ok((merges, vocab.into()))
+}
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _minbpe(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_stats, m)?)?;
+    m.add_function(wrap_pyfunction!(merge, m)?)?;
+    m.add_function(wrap_pyfunction!(train_basic_bpe, m)?)?;
     Ok(())
 }
